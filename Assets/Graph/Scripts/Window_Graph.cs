5b5f59a2bff6a71131366da9f25b5bd0cb3585a5
zunaira-dev <zunaira.mash30@gmail.com>
2022-09-23 10:18:54 +0500

    Graph

---
+++ Assets/Graph/Scripts/Window_Graph.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CodeMonkey.Utils;
+
+public class Window_Graph : MonoBehaviour {
+
+    [SerializeField] private Sprite circleSprite;
+    public RectTransform graphContainer;
+
+    private void Awake() {
+        List<float> valueList = new List<float>() { 5, 2, 12, 15, 20, 22, 30, 15, 13, 17, 25, 37, 40, 36, 33 ,0};
+       StartCoroutine( ShowGraph(valueList));
+    }
+
+    private GameObject CreateCircle(Vector2 anchoredPosition) {
+        GameObject gameObject = new GameObject("circle", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().sprite = circleSprite;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = anchoredPosition;
+        rectTransform.sizeDelta = new Vector2(11, 11);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        return gameObject;
+    }
+
+    IEnumerator ShowGraph(List<float> valueList) {
+        float graphHeight = graphContainer.sizeDelta.y;
+        float yMaximum = 100f;
+        float xSize = 50f;
+
+        GameObject lastCircleGameObject = null;
+        int i = 0;
+        while(i<valueList.Count) {
+        //for (int i = 0; i < valueList.Count; i++) {
+            float xPosition = xSize + i * xSize;
+            float yPosition = (valueList[i] / yMaximum) * graphHeight;
+            GameObject circleGameObject = CreateCircle(new Vector2(xPosition, yPosition));
+            if (lastCircleGameObject != null) {
+                CreateDotConnection(lastCircleGameObject.GetComponent<RectTransform>().anchoredPosition, circleGameObject.GetComponent<RectTransform>().anchoredPosition);
+            }
+            lastCircleGameObject = circleGameObject;
+            i++;
+            yield return new WaitForSeconds(1);
+        }
+    }
+    private void CreateDotConnection(Vector2 dotPositionA, Vector2 dotPositionB) {
+        GameObject gameObject = new GameObject("dotConnection", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().color = new Color(1,1,1, .5f);
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        Vector2 dir = (dotPositionB - dotPositionA).normalized;
+        float distance = Vector2.Distance(dotPositionA, dotPositionB);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        rectTransform.sizeDelta = new Vector2(distance, 3f);
+        rectTransform.anchoredPosition = dotPositionA + dir * distance * .5f;
+        rectTransform.localEulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(dir));
+    }
+
+}
---
+++ Assets/RealtimeDatabase/realtimedatabase.cs
@@ -0,0 +1,67 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using Firebase.Database;
+//using UnityEngine.UI;
+
+//public class realtimedatabase : MonoBehaviour
+//{
+//    DatabaseReference reference;
+//    [SerializeField] InputField username;
+//    [SerializeField] InputField email;
+//    [SerializeField] InputField nametoread;
+//    [SerializeField] Text data;
+  
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        reference = FirebaseDatabase.DefaultInstance.RootReference;
+//    }
+//    User user = new User();
+//    public void savedata()
+//    {
+
+//        user.UserName = username.text;
+//        user.Email = email.text;
+//        string json = JsonUtility.ToJson(user);
+
+//        reference.Child("User").Child(user.UserName).SetRawJsonValueAsync(json).ContinueWith(task =>
+//        {
+//            if (task.IsCompleted)
+//            {
+//                Debug.Log("successfully added data to firebase");
+//            }
+//            else
+//            {
+//                Debug.Log("not successfull");
+//            }
+//        });
+//    }
+
+// public void Read_Data()
+//    {
+//        reference.Child("User").Child(nametoread.text).GetValueAsync().ContinueWith(task =>
+//        {
+//            if (task.IsCompleted)
+//            {
+//                Debug.Log("successfull");
+//                DataSnapshot snapshot = task.Result;
+//               Debug.Log( snapshot.Child("UserName").Value.ToString());
+//                Debug.Log(snapshot.Child("Email").Value.ToString());
+
+//            }
+//            else
+//            {
+//                Debug.Log("not successfull");
+//            }
+//        });
+//    }
+
+   
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+        
+//    }
+//}
